af3178021566d7ed892a7cb58b537513b26c40e1
blueshirt233 <2644536256@qq.com>
2026-06-02 21:40:02 +0800

    again

--- src/config.ts
+++ src/config.ts
@@ -446,7 +446,7 @@ export const commentConfig: CommentConfig = {
 	enable: true, // 启用评论功能。当设置为 false 时，评论组件将不会显示在文章区域。
 	system: "twikoo", // 评论系统选择: "twikoo" | "giscus"
 	twikoo: {
-		envId: "https://twikoo-indol-phi.vercel.app/",
+		envId: "https://twikoo.2644536256.date/",
 		lang: SITE_LANG,
 	},
 	giscus: {
