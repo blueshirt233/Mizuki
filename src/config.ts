cb7e8e6647247045feb25a860d5b895fa0184300
blueshirt233 <2644536256@qq.com>
2026-06-14 16:57:16 +0800

    again

--- src/config.ts
+++ src/config.ts
@@ -483,13 +483,14 @@ export const announcementConfig: AnnouncementConfig = {
 
 export const musicPlayerConfig: MusicPlayerConfig = {
 	enable: true, // 启用音乐播放器功能
-	showFloatingPlayer: true, // 显示悬浮播放器 UI
+	showFloatingPlayer: false, // 显示悬浮播放器 UI
 	floatingEntryMode: "fab", // 悬浮入口模式："default" 为独立悬浮播放器，"fab" 为集成到通用 FAB 组
 	mode: "meting", // 音乐播放器模式，可选 "local" 或 "meting"
 	meting_api:
-		"https://meting.mikus.ink/api?server=tencent&type=playlist&id=8941221193", // Meting API 地址
-	id: "8941221193", // 歌单ID
-	server: "tencent", // 音乐源服务器。有的meting的api源支持更多平台,一般来说,netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
+		// "https://meting.mikus.ink/api?server=tencent&type=playlist&id=8941221193",
+		"https://meting.mikus.ink/api?server=netease&type=playlist&id=907088656",// Meting API 地址
+	id: "907088656", // 歌单ID
+	server: "netease", // 音乐源服务器。有的meting的api源支持更多平台,一般来说,netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
 	type: "playlist", // 播单类型
 };
 
